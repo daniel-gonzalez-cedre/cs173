3c4c4b8f79f476ddeb753ed1609374788731392c
cedré <39937561+daniel-gonzalez-cedre@users.noreply.github.com>
2026-01-20 18:32:43 -0600

    Changes to be committed: 	modified:   index.html 	new file:   logistics/office_hours.html 	renamed:    staff.html -> logistics/staff.html 	deleted:    office_hours.html 	new file:   problem_sets.html 	new file:   resources/lecture_notes.html 	new file:   resources/lecture_notes/book.pdf 	new file:   resources/lecture_notes/chapter_00.pdf 	new file:   resources/lecture_notes/chapter_01.pdf 	new file:   resources/lecture_notes/update.sh 	modified:   style.css

---
+++ resources/lecture_notes/update.sh
@@ -0,0 +1,3 @@
+cp -v ~/academia/pedagogy/book_typst/book.pdf ./book.pdf
+cp -v ~/academia/pedagogy/book_typst/chapter_00/chapter.pdf ./chapter_00.pdf
+cp -v ~/academia/pedagogy/book_typst/chapter_01/chapter.pdf ./chapter_01.pdf
